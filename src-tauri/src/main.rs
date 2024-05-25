4a9fd4b29a739a7ad6aa93a061065640bde76971
gagahsyuja <gghssa1@gmail.com>
2024-05-25 22:30:03 +0700

    Added more invoke from read module

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,25 @@
 
 mod installer;
 use installer::partitions;
+use installer::read::*;
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![partitions::get_disk_info])
+    .invoke_handler(tauri::generate_handler![
+        partitions::get_disk_info,
+        battery::get_battery_percentage,
+        lspci::lspci,
+        memory::get_memory_capacity,
+        memory::get_memory_free,
+        model::get_model_number,
+        model::get_brand,
+        os::get_other_os,
+        storage::get_storage_capacity,
+        storage::get_partitions,
+        storage::get_flags,
+        storage::get_mountpoints,
+        storage::get_filesystem
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
