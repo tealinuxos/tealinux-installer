bbd82c3dd1a1cf0d5f6ea22e63364a2581bae405
gagahsyuja <gghssa1@gmail.com>
2024-05-30 20:08:07 +0700

    Much simpler invoke handler

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,26 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod installer;
-use installer::partitions;
-use installer::read::*;
+mod api;
+mod read;
+
+use api::get_read_json;
 
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![
-        partitions::get_disk_info,
-        battery::get_battery_percentage,
-        lspci::lspci,
-        memory::get_memory_capacity,
-        memory::get_memory_free,
-        model::get_model_number,
-        model::get_brand,
-        os::get_other_os,
-        storage::get_storage_capacity,
-        storage::get_partitions,
-        storage::get_flags,
-        storage::get_mountpoints,
-        storage::get_filesystem
+        get_read_json
     ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
