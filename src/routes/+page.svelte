85a43b7fe57250a9c03ccbbefe92431ecdeda296
gagahsyuja <gghssa1@gmail.com>
2024-05-30 20:09:03 +0700

    Parsing JSON to object

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,27 @@
 <script>
+
     import { invoke } from "@tauri-apps/api/tauri";
 
-    async function getDiskInfo() {
-        let disks = await invoke('get_disk_info');
-        console.log(disks);
+    let json;
+
+    let showModelName = false;
+
+    async function handleGetJSON() {
+
+        invoke('get_read_json').then((response) => {
+            json = JSON.parse(response);
+            console.log(json);
+            showModelName = true;
+        }).await
     }
+
 </script>
 
-<h1 class="text-red-600">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button on:click="{getDiskInfo}">Disk Info</button>
+<div class="flex flex-col justify-center p-5"> 
+    <button class="bg-black text-white" on:click="{handleGetJSON}">
+        Get JSON
+    </button>
+    {#if showModelName}
+        <h1>{json.model.brandName}</h1>
+    {/if}
+</div>
