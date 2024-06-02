3dce83ae99a9d77cd6b315a8ce4da157fd249a6b
gagahsyuja <gghssa1@gmail.com>
2024-06-02 18:45:12 +0700

    Checking sudo privileges using crate users

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,22 @@ mod api;
 mod read;
 
 use api::get_read_json;
+use users::get_current_uid;
 
-fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![
-        get_read_json
-    ])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+fn main()
+{
+    match get_current_uid()
+    {
+        0 => {
+            tauri::Builder::default()
+                .invoke_handler(tauri::generate_handler![
+                    get_read_json
+                ])
+                .run(tauri::generate_context!())
+                .expect("error while running tauri application");
+        }
+        _ => {
+            println!("Run with sudo:\nsudo -E bunx tauri dev");
+        }
+    }
 }
