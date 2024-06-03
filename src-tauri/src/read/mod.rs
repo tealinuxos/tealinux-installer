18971dfd6a36688ad256e2fdd2fa2c7ada336c5e
gagahsyuja <gghssa1@gmail.com>
2024-06-03 22:47:57 +0700

    Using crate parted-information-extractor-lib for getting storage information

--- src-tauri/src/read/mod.rs
+++ src-tauri/src/read/mod.rs
@@ -5,22 +5,21 @@ pub mod storage;
 pub mod battery;
 pub mod online;
 
-use serde::{ Serialize, Deserialize };
+use serde::Serialize;
 use model::Model;
 use memory::Memory;
 use battery::Battery;
 use online::Online;
 use pci::Pci;
-use storage::disk::Disk;
-use storage::partition::Partition;
+use parted_information_extractor_lib::gagah_disk::{ Disk, get_disk_from_json::get_all_disk };
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
 #[serde(rename_all="camelCase")]
 pub struct Read
 {
     model: Model,
     memory: Memory,
-    storage: Disk,
+    disks: Vec<Disk>,
     operating_systems: Vec<String>,
     battery: Battery,
     online: Online,
@@ -29,9 +28,9 @@ pub struct Read
 
 impl Read
 {
-    pub fn new(model: Model, memory: Memory, storage: Disk, operating_systems: Vec<String>, battery: Battery, online: Online, lspci: Pci) -> Self
+    pub fn new(model: Model, memory: Memory, disks: Vec<Disk>, operating_systems: Vec<String>, battery: Battery, online: Online, lspci: Pci) -> Self
     {
-        Self { model, memory, storage, operating_systems, battery, online, lspci }
+        Self { model, memory, disks, operating_systems, battery, online, lspci }
     }
 }
 
@@ -43,17 +42,12 @@ pub fn get_read() -> Read
     // Memory
     let memory = Memory::new();
 
-    // Partitions
-    let vec_partitions = vec![
-        Partition::new("/dev/sda1".to_string(), 2048, 1050624, 1048576, 512, "ext4".to_string()),
-        Partition::new("null".to_string(), 1050625, 5244929, 4194304, 2048, "free".to_string()),
-        Partition::new("/dev/sda2".to_string(), 5244930, 22022146, 16777216, 8192, "ext4".to_string())
-    ];
+    // Operating Systems
+    let operating_systems = vec!["Windows 10".to_string(), "Arch Linux".to_string()];
 
     // Storage
-    let storage = Disk::new("/dev/sda".to_string(), "gpt".to_string(), vec_partitions);
-
-    let operating_systems = vec!["Windows 10".to_string(), "Arch Linux".to_string()];
+    let json = get_all_disk();
+    let storage = Disk::from_value_vec(json);
 
     // Battery
     let battery = Battery::new();
