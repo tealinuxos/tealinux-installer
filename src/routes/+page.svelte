495a1a246387d612b6a62e47d7a9efd3f7b9f982
Fadhil Firmansyah <fadhilfirmansyah100@gmail.com>
2024-06-15 12:01:22 +0700

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,113 @@
+<script>
+    import "../app.css"
+  </script>
+  
+  <div>
+    <h1 class="mt-4 text-center font-mono text-4xl">Welcome into <span class="bg-green-500">Tea</span>Linux Installer</h1>
+    
+    <div class="mt-10 grid grid-cols-3 gap-5 place-items-center">
+      
+      <div class="ml-14">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="flex items-center space-x-4">
+            <img src="/icon/keyboard.svg" alt="Keyboard" class="h-14">
+            <h5 class="mt-5 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Keyboard</h5>
+          </div>
+          
+          <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nesciunt laudantium</p>
+        </a>
+      </div>
+
+      <div>
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="flex items-center space-x-4">
+            <img src="/icon/language.svg" alt="Language Support" class="h-14">
+            <h5 class="mt-2 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Language Support</h5>
+          </div>
+          
+          <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nesciunt laudantium</p>
+        </a>
+      </div>
+
+      <div class="mr-14">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="flex items-center space-x-4">
+            <img src="/icon/time.svg" alt="Time & Date" class="h-14">
+            <h5 class="mt-1 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Time & Date</h5>
+          </div>
+          
+          <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nesciunt laudantium</p>
+        </a>
+      </div>
+      <!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+      <div class="ml-14">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="flex items-center space-x-4">
+            <img src="/icon/disk.svg" alt="Partition" class="h-14">
+            <h5 class="mt-2 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Installation Destination</h5>
+          </div>
+          
+          <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nesciunt laudantium</p>
+        </a>
+      </div>
+
+      <div>
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="flex items-center space-x-4">
+            <img src="/icon/network.svg" alt="Network & Host Name" class="h-14">
+            <h5 class="mt-3 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Network & Host Name</h5>
+          </div>
+          
+          <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nesciunt laudantium</p>
+        </a>
+      </div>
+
+      <div class="mr-14">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="flex items-center space-x-4">
+            <img src="/icon/security.svg" alt="Time & Date" class="h-14">
+            <h5 class="mt-3 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Security Profile</h5>
+          </div>
+          
+          <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nesciunt laudantium</p>
+        </a>
+      </div>
+      <!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+      <div class="ml-14">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="flex items-center space-x-4">
+            <img src="/icon/user.svg" alt="User Creation" class="h-14">
+            <h5 class="mt-2 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">User Creation</h5>
+          </div>
+          
+          <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nesciunt laudantium</p>
+        </a>
+      </div>
+
+      <div>
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div class="flex items-center space-x-4">
+            <img src="/icon/root.svg" alt="Root Password" class="h-14">
+            <h5 class="mt-1 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Root Password</h5>
+          </div>
+          
+          <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque nesciunt laudantium</p>
+        </a>
+      </div>
+
+      <div class="mr-14">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <p class="font-normal text-gray-700 dark:text-gray-400 mx-32">Keyboard = <br>Language = <br>Time = <br>Partition = <br>Network = <br>Security = <br>User = </p>
+        </a>
+      </div>
+    </div>
+  </div>
