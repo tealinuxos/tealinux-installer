6b126b1f08ab83abf0f4e13fe73bea12960c19b3
gagahsyuja <gghssa1@gmail.com>
2024-06-16 17:14:23 +0700

    Cleaning up from the deleted modules

--- src-tauri/src/installer/mod.rs
+++ src-tauri/src/installer/mod.rs
@@ -1,7 +1,18 @@
-pub mod commands;
-pub mod utils;
-pub mod fstab;
-pub mod timezone;
-pub mod internet;
-pub mod partitions;
-pub mod read;
+pub mod blueprint;
+
+use std::fs::File;
+use std::io::BufReader;
+use tea_arch_chroot_lib::prechroot::*;
+
+pub use self::blueprint::BluePrint;
+
+#[tauri::command]
+pub async fn start_install()
+{
+    let file = File::open("/opt/installer.json").unwrap();
+    let reader = BufReader::new(file);
+
+    let json: BluePrint = serde_json::from_reader(reader).unwrap();
+
+    println!("{:#?}", json);
+}
