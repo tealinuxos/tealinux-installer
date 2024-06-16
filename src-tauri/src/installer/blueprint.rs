577f010b04b61782d3f02024a93333abd7dda8b2
gagahsyuja <gghssa1@gmail.com>
2024-06-16 17:12:38 +0700

    Add blueprint struct

---
+++ src-tauri/src/installer/blueprint.rs
@@ -0,0 +1,22 @@
+use serde::{ Serialize, Deserialize };
+use tea_arch_chroot_lib::chroot::Account;
+use tea_arch_chroot_lib::chroot::Timezone;
+use tea_arch_chroot_lib::chroot::Locale;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BluePrint
+{
+    pub account: Account,
+    pub locale: Locale,
+    pub timezone: Timezone,
+    // pub partition: Vec<Partition>
+}
+
+// #[derive(Debug, Serialize, Deserialize)]
+// pub struct Partition
+// {
+//     pub path: String,
+//     pub filesystem: String,
+//     pub mountpoints: Option<Vec<String>>,
+//     pub format: Option<bool>
+// }
