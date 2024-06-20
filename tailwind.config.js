92570c90481f740d4463feebb2a7b81e9d95dca5
LiXuang12 <muhfarisassami@gmail.com>
2024-06-20 22:46:10 +0700

    update welcome page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greenTealinux: "#26A768",
+      }
+    },
   },
   plugins: [],
 }
