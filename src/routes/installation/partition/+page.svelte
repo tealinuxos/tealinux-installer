fe8ee7d48564332cd314051037a36733792fa551
gagahsyuja <gghssa1@gmail.com>
2024-06-24 16:02:14 +0700

    Fix partitioning page not able to change disk

--- src/routes/installation/partition/+page.svelte
+++ src/routes/installation/partition/+page.svelte
@@ -4,75 +4,90 @@
     import { onMount } from "svelte";
     import { json } from "../stores.js";
 
-    let disks;
-    let ready = false;
-    let showPartition = false;
     let selectedDisk = 0;
     let partitionDetail = [];
+    let filesystems;
 
     const getStorageJSON = async () => {
-        invoke('get_storage_json').then((response) => {
-            disks = JSON.parse(response);
-            console.log(disks[selectedDisk].partitions);
-            ready = true;
-            handlePartitionDetail();
+
+        let disks = await invoke('get_storage_json');
+
+        return JSON.parse(disks);
+    };
+
+    const getFilesystemJSON = () => {
+        invoke('get_filesystem_json').then((response) => {
+            filesystems = JSON.parse(response);
         });
     };
 
-    const handlePartitionDetail = () => {
+    const handlePartitionDetail = async (disks, selectedDisk) => {
+
+        partitionDetail = []
+
         for (let i of disks[selectedDisk].partitions.keys())
         {
             let partitionPath = disks[selectedDisk].partitions[i].partitionPath;
 
             partitionDetail.push({
                 path: partitionPath,
-                format: false,
+                format: null,
                 mountpoint: null
             });
         }
-        console.log(partitionDetail);
+
+        return partitionDetail;
     }
 
     const handleSetPartition = () => {
         json.partition = partitionDetail;
     }
 
-    $: console.log(partitionDetail);
-
     onMount(() => {
         getStorageJSON();
+        getFilesystemJSON();
     });
 
 </script>
 
 <h1>Partitioning</h1>
 
-{#if ready}
-<div class="flex flex-col">
-    <select class="w-1/3" bind:value={selectedDisk} on:change={handlePartitionDetail}>
-        {#each disks as disk, i}
-            {@const model = disk.model}
-            {@const path = disk.diskPath}
-            <option value={i}>{model + ' (' + path + ')'}</option>
-        {/each}
-    </select>
+{#await getStorageJSON()}
+    Loading...
+{:then disks}
+    <div class="flex flex-col">
+        <select class="w-1/3" bind:value={selectedDisk}>
+            {#each disks as disk, i}
+                {@const model = disk.model}
+                {@const path = disk.diskPath}
+                <option value={i}>{model + ' (' + path + ')'}</option>
+            {/each}
+        </select>
+    </div>
 
-    {#each disks[selectedDisk].partitions as partition, i}
-        {@const path = partition.partitionPath}
-        <div class="flex flex-row">
-            <span>{path}</span>
-            <select bind:value={partitionDetail[i].format}>
-                <option value={false}>Do not format</option>
-                <option value={true}>Format</option>
-            </select>
-            <select bind:value={partitionDetail[i].mountpoint}>
-                <option value={null}>No Mountpoint</option>
-                <option value="/">/</option>
-                <option value="/boot">/boot</option>
-                <option value="/home">/home</option>
-            </select>
+    {#await handlePartitionDetail(disks, selectedDisk)}
+        Loading...
+    {:then partitions}
+        <div class="flex flex-col">
+            {#each partitions as partition, i}
+                {@const path = partition.path}
+                <div class="flex flex-row">
+                    <span>{path}</span>
+                    <select bind:value={partitionDetail[i].format}>
+                        <option value={null}>Do not format</option>
+                        {#each filesystems as filesystem}
+                            <option value={filesystem}>{filesystem}</option>
+                        {/each}
+                    </select>
+                    <select bind:value={partitionDetail[i].mountpoint}>
+                        <option value={null}>No Mountpoint</option>
+                        <option value="/">/</option>
+                        <option value="/boot">/boot</option>
+                        <option value="/home">/home</option>
+                    </select>
+                </div>
+            {/each}
         </div>
-    {/each}
-</div>
-<a href="/installation/summary" on:click={handleSetPartition}>Next</a>
-{/if}
+        <a href="/installation/summary" on:click={handleSetPartition}>Next</a>
+    {/await}
+{/await}
