cad1a41653806b220e07e5b39f22655161e8585f
gagahsyuja <gghssa1@gmail.com>
2024-06-24 15:57:07 +0700

    Fix storage to handle format properly

--- src-tauri/src/storage/mod.rs
+++ src-tauri/src/storage/mod.rs
@@ -1,7 +1,77 @@
 use tea_partition_api_lib::{ Disk, Partition };
 use tea_partition_api_lib::read::get_partition;
+use std::io::Error;
+use duct::cmd;
+
+pub mod filesystem;
 
 pub async fn get_storage() -> Vec<Disk>
 {
     get_partition::parted_list_partition()
+}
+
+pub async fn format(filesystem: String, path: String) -> Result<(), Error>
+{
+    let command = {
+
+        if filesystem == "btrfs"
+        {
+            format!("mkfs.btrfs --force {}", path)
+        }
+        else if filesystem == "exfat"
+        {
+            format!("mkfs.{} {}", filesystem, path)
+        }
+        else if filesystem.contains("fat")
+        {
+            let fat = filesystem.trim_matches(|c| c == 'f' || c == 'a' || c == 't');
+
+            format!("mkfs.fat -F {} {}", fat, path)
+        }
+        else if filesystem.contains("ext")
+        {
+            format!("mkfs.{} -F {}", filesystem, path)
+        }
+        else
+        {
+            format!("mkfs.{} {}", filesystem, path)
+        }
+    };
+
+    let command: Vec<String> = command.split_whitespace().map(|s| s.to_string()).collect();
+
+    cmd(&command[0], &command[1..]).run()?;
+
+    Ok(())
+}
+
+pub async fn mount(partition_path: String, mountpoint: String, options: Option<Vec<String>>) -> Result<(), Error>
+{
+    let options = {
+        if options.is_some()
+        {
+            let options: String = options
+                .unwrap()
+                .iter()
+                .map(|o| format!("{},", o))
+                .collect();
+
+            Some(options)
+        }
+        else
+        {
+            None
+        }
+    };
+
+    if options.is_none()
+    {
+        cmd!("mount", partition_path, mountpoint).run()?;
+    }
+    else
+    {
+        cmd!("mount", "-o", options.unwrap(), partition_path, mountpoint).run()?;
+    }
+
+    Ok(())
 }
