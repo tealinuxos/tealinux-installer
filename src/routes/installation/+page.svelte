8f7f81fd5c25ec72ef16203452b1a7ff86b8e2f4
naufaladrian <naufal.adrian904@gmail.com>
2024-06-25 12:33:41 +0700

    add poppin-semibold; fix installation

--- src/routes/installation/+page.svelte
+++ src/routes/installation/+page.svelte
@@ -25,30 +25,45 @@
 	});
 </script>
 
-<nav class="flex flex-col flex-1 overflow-y-auto bg-white">
-	<div class="bg-[#D9D9D9] flex items-center py-4 px-8 w-full font-sans font-medium text-2xl">
-		Tealinux Installer
-	</div>
-</nav>
-
-<main class="p-8">
-	<div
-		class="bg-[#D9D9D9] w-full grid-cols-3 grid place-items-center py-3 px-16 h-[40vh] rounded-2xl"
-	>
-		<div class="border border-black flex flex-col items-center gap-y-1">
-			<!-- <img src="{laptop}" alt="gambar laptop" class="w-1/2"> -->
-			<p class="text-2xl font-medium">82SV</p>
-			<p>Yoga Slim 7Pro 14|AP7</p>
-			<p>80 %</p>
-			<p>online</p>
+<main class="ax-h-dvh">
+	<div class="p-8 overflow-auto max-h-[85dvh]">
+		<div class=" bg-[#D9D9D9] bg-opacity-45 w-full p-5 rounded-2xl mb-6">
+			<div class="bg-[#DCDCDC] grid-cols-3 grid place-items-center py-3 px-16 h-[35vh] rounded-3xl">
+				<div class="border border-black flex flex-col items-center gap-y-1">
+					<p class="text-2xl font-medium">82SV</p>
+					<p>Yoga Slim 7Pro 14|AP7</p>
+					<p>80 %</p>
+					<p>online</p>
+				</div>
+				<div>ini bagian tengah</div>
+				<div>ini bagian kanan</div>
+			</div>
 		</div>
-		<div>ini bagian tengah</div>
-		<div>ini bagian kanan</div>
-	</div>
-	<div class="w-full mt-8 py-4 grid grid-cols-2 gap-8 max-h-96 overflow-y-auto">
-		{#each [1, 2, 3, 4, 5, 6] as e}
-			<div class="aspect-square bg-[#D9D9D9]"></div>
+		{#each [1, 2] as x}
+			<div class=" bg-[#D9D9D9] bg-opacity-45 w-full p-5 rounded-2xl mb-6">
+				<div
+					class="bg-[#DCDCDC] grid-cols-3 grid place-items-center py-3 px-16 h-[35vh] rounded-3xl"
+				></div>
+			</div>
 		{/each}
 	</div>
-	<a href="/installation/locale">Install</a>
+
+	<div class="flex justify-between h-[15dvh] items-center w-full px-8">
+		<button
+			on:click={() => {
+				window.location.href = '/';
+			}}
+			type="button"
+			class="py-[10px] px-[20px] bg-[#929AAB] text-white font-poppin text-base rounded-lg"
+			>Back</button
+		>
+		<button
+			on:click={() => {
+				window.location.href = '/installation/locale';
+			}}
+			type="button"
+			class="py-[10px] px-[20px] bg-[#929AAB] text-white font-poppin text-base rounded-lg"
+			>Next</button
+		>
+	</div>
 </main>
