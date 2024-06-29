580e663f7d778232cf00d4f31803fead9e80a6ba
naufaladrian <naufal.adrian904@gmail.com>
2024-06-29 22:16:37 +0700

    add accent color

--- src/routes/installation/+page.svelte
+++ src/routes/installation/+page.svelte
@@ -27,8 +27,8 @@
 
 <main class="ax-h-dvh">
 	<div class="p-8 overflow-auto max-h-[85dvh]">
-		<div class=" bg-[#D9D9D9] bg-opacity-45 w-full p-5 rounded-2xl mb-6">
-			<div class="bg-[#DCDCDC] grid-cols-3 grid place-items-center py-3 px-16 h-[35vh] rounded-3xl">
+		<div class=" bg-greenTealinux bg-opacity-25 w-full p-5 rounded-2xl mb-6">
+			<div class="bg-white grid-cols-3 grid place-items-center py-3 px-16 h-[35vh] rounded-3xl">
 				<div class="border border-black flex flex-col items-center gap-y-1">
 					<p class="text-2xl font-medium">82SV</p>
 					<p>Yoga Slim 7Pro 14|AP7</p>
@@ -40,26 +40,24 @@
 			</div>
 		</div>
 		{#each [1, 2] as x}
-			<div class=" bg-[#D9D9D9] bg-opacity-45 w-full p-5 rounded-2xl mb-6">
+			<div class=" bg-greenTealinux bg-opacity-25 w-full p-5 rounded-2xl mb-6">
 				<div
-					class="bg-[#DCDCDC] grid-cols-3 grid place-items-center py-3 px-16 h-[35vh] rounded-3xl"
+					class="bg-white grid-cols-3 grid place-items-center py-3 px-16 h-[35vh] rounded-3xl"
 				></div>
 			</div>
 		{/each}
 	</div>
 
-	<div class="flex justify-between h-[15dvh] items-center w-full px-8">
-		<button
-			on:click={() => {
-				window.location.href = '/';
-			}}
-			type="button"
-			class="py-[10px] px-[20px] bg-[#929AAB] text-white font-poppin text-base rounded-lg"
-			>Back</button
+	<div class="flex justify-between h-[15dvh] items-center w-full px-8 font-poppin">
+		<a
+			href="/"
+			class="py-[10px] px-[20px] bg-greenTealinux text-white font-poppin text-base rounded-lg"
+			>Back</a
 		>
 		<a
 			href="/installation/keyboard"
-			class="py-[10px] px-[20px] bg-[#929AAB] text-white font-poppin text-base rounded-lg">Next</a
+			class="py-[10px] px-[20px] bg-greenTealinux text-white font-poppin text-base rounded-lg"
+			>Next</a
 		>
 	</div>
 </main>
