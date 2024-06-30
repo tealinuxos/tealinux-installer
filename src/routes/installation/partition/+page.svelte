db3106faa030ba3bb7d8dfb7a712ebe55a3582a3
gagahsyuja <gghssa1@gmail.com>
2024-07-01 00:30:58 +0700

    Make changes directly to json file and handling async with await

--- src/routes/installation/partition/+page.svelte
+++ src/routes/installation/partition/+page.svelte
@@ -3,22 +3,23 @@
     import { invoke } from "@tauri-apps/api/tauri";
     import { onMount } from "svelte";
     import { json } from "../stores.js";
+    import { getRead } from "../global.js";
 
     let selectedDisk = 0;
     let partitionDetail = [];
-    let filesystems;
 
     const getStorageJSON = async () => {
 
-        let disks = await invoke('get_storage_json');
+        let json = await getRead();
 
-        return JSON.parse(disks);
+        return json.disk;
     };
 
-    const getFilesystemJSON = () => {
-        invoke('get_filesystem_json').then((response) => {
-            filesystems = JSON.parse(response);
-        });
+    const getFilesystemJSON = async () => {
+
+        let filesystems = await invoke('get_filesystem_json');
+
+        return JSON.parse(filesystems);
     };
 
     const handlePartitionDetail = async (disks, selectedDisk) => {
@@ -28,25 +29,27 @@
         for (let i of disks[selectedDisk].partitions.keys())
         {
             let partitionPath = disks[selectedDisk].partitions[i].partitionPath;
+            let startSector = disks[selectedDisk].partitions[i].start.slice(0, -1);
+            let endSector = disks[selectedDisk].partitions[i].end.slice(0, -1);
 
             partitionDetail.push({
                 path: partitionPath,
                 format: null,
-                mountpoint: null
+                mountpoint: null,
+                start: parseInt(startSector),
+                end: parseInt(endSector)
             });
         }
 
         return partitionDetail;
-    }
+    };
 
-    const handleSetPartition = () => {
-        json.partition = partitionDetail;
-    }
+    const handleSetPartition = async () => {
 
-    onMount(() => {
-        getStorageJSON();
-        getFilesystemJSON();
-    });
+        let partition = JSON.stringify(partitionDetail);
+
+        await invoke("blueprint_set_partition", { partition });
+    };
 
 </script>
 
@@ -71,21 +74,48 @@
         <div class="flex flex-col">
             {#each partitions as partition, i}
                 {@const path = partition.path}
-                <div class="flex flex-row">
-                    <span>{path}</span>
-                    <select bind:value={partitionDetail[i].format}>
-                        <option value={null}>Do not format</option>
-                        {#each filesystems as filesystem}
-                            <option value={filesystem}>{filesystem}</option>
-                        {/each}
-                    </select>
-                    <select bind:value={partitionDetail[i].mountpoint}>
-                        <option value={null}>No Mountpoint</option>
-                        <option value="/">/</option>
-                        <option value="/boot">/boot</option>
-                        <option value="/home">/home</option>
-                    </select>
-                </div>
+
+                {#if path == null}
+                    <div class="flex flex-row">
+                        <span>Unallocated</span>
+                        <select bind:value={partitionDetail[i].format} on:change={handleSetPartition}>
+                            <option value={null}>Do not format</option>
+                            {#await getFilesystemJSON()}
+                                <option disabled={true}>Loading...</option>
+                            {:then filesystems}
+                                {#each filesystems as filesystem}
+                                    <option value={filesystem}>{filesystem}</option>
+                                {/each}
+                            {/await}
+                        </select>
+                        <select bind:value={partitionDetail[i].mountpoint} on:change={handleSetPartition}>
+                            <option value={null}>No Mountpoint</option>
+                            <option value="/">/</option>
+                            <option value="/boot">/boot</option>
+                            <option value="/home">/home</option>
+                        </select>
+                    </div>
+                {:else}
+                    <div class="flex flex-row">
+                        <span>{path}</span>
+                        <select bind:value={partitionDetail[i].format} on:change={handleSetPartition}>
+                            <option value={null}>Do not format</option>
+                            {#await getFilesystemJSON()}
+                                <option disabled={true}>Loading...</option>
+                            {:then filesystems}
+                                {#each filesystems as filesystem}
+                                    <option value={filesystem}>{filesystem}</option>
+                                {/each}
+                            {/await}
+                        </select>
+                        <select bind:value={partitionDetail[i].mountpoint} on:change={handleSetPartition}>
+                            <option value={null}>No Mountpoint</option>
+                            <option value="/">/</option>
+                            <option value="/boot/efi">/boot/efi</option>
+                            <option value="/home">/home</option>
+                        </select>
+                    </div>
+                {/if}
             {/each}
         </div>
         <a href="/installation/account" class="pr-4">back</a>
