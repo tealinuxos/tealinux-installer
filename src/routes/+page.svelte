a582b96ccede49c63de977a572950e16fe16ed4d
gagahsyuja <gghssa1@gmail.com>
2024-07-01 00:34:35 +0700

    Preparing empty json file and wait until the data is ready

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,49 +3,54 @@
 	import { onMount } from 'svelte';
 	import Tealinux from '$lib/assets/Vector.png';
 
-	let online = false;
+    const isOnline = async () => {
 
-	const isOnline = async () => {
-		invoke('is_online').then((status) => (online = status));
-	};
-
-	function toLocale() {
-		if (online) {
-			window.location.href = '/installation';
-		}
-	}
+        let online = await invoke('is_online');
+        return online;
+    }
 
     const setReadJSON = async () => {
         await invoke("set_read_json");
     };
 
+    const setBlueprintJSON = async () => {
+        await invoke("set_empty_blueprint");
+    };
+
 	onMount(() => {
 		isOnline();
-        setReadJSON();
+        setBlueprintJSON();
 	});
 </script>
 
 <div class="flex items-center justify-center min-h-screen">
 	<div class="text-center">
 		<img src={Tealinux} alt="" class="mx-auto mb-4" />
-		<h1 class="font-archivo font-semibold text-6xl -tracking-[4.5%] mb-4">Welcome to Tealinux</h1>
-		<h2 class="font-poppin mb-4 text-4xl tracking-normal">Press start to install</h2>
 
-		<div class="p-2">
-			{#if !online}
-				<h3 class="mb-2 text-red-600 font-bold">Please connect to internet!</h3>
-				<a href="/installation/locale">Bypass Internet</a>
-			{/if}
-			<br />
-			<div>
-				<button
-					on:click={toLocale}
-					class=" bg-greenTealinux rounded-3xl hover:bg-green-800 text-white font-poppinsemibold text-xl py-[10px] px-28 disabled:bg-green-400 border-2 border-black/15"
-					disabled={!online}
-				>
-					START
-				</button>
-			</div>
-		</div>
+        {#await setReadJSON()}
+        {:then}
+            <h1 class="font-archivo font-semibold text-6xl -tracking-[4.5%] mb-4">Welcome to Tealinux</h1>
+            <h2 class="font-poppin mb-4 text-4xl tracking-normal">Press start to install</h2>
+
+            <div class="p-2">
+                {#await isOnline()}
+                {:then online}
+                    {#if !online}
+                        <h3 class="mb-2 text-red-600 font-bold">Please connect to internet!</h3>
+                        <a href="/installation/locale">Bypass Internet</a>
+                    {/if}
+                    <br />
+                    <div>
+                        <button
+                            on:click={() => window.location.href = '/installation'}
+                            class=" bg-greenTealinux rounded-3xl hover:bg-green-800 text-white font-poppinsemibold text-xl py-[10px] px-28 disabled:bg-green-400 border-2 border-black/15"
+                            disabled={!online}
+                        >
+                            START
+                        </button>
+                    </div>
+                {/await}
+            </div>
+        {/await}
 	</div>
 </div>
