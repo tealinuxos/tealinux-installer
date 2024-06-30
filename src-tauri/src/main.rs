6f88b1f6f79c70419b22058c4700d7eb9e5de91f
gagahsyuja <gghssa1@gmail.com>
2024-07-01 00:24:48 +0700

    Add more invokeable function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,9 +9,16 @@ mod installer;
 mod system;
 
 use api::*;
+use api::locale::*;
+use api::timezone::*;
+use api::keyboard::*;
+use api::account::*;
+use api::partition::*;
 use system::reboot::reboot;
 use users::get_current_uid;
 use installer::{ start_install, is_online, print_json };
+
+use duct::cmd;
 
 fn main()
 {
@@ -21,16 +28,23 @@ fn main()
             tauri::Builder::default()
                 .invoke_handler(tauri::generate_handler![
                     get_read_json,
-                    set_blueprint_json,
+                    set_empty_blueprint,
+                    blueprint_set_locale,
+                    blueprint_set_timezone,
+                    blueprint_set_account,
+                    blueprint_set_partition,
                     set_read_json,
                     get_locale_json,
                     get_timezone_json,
                     start_install,
                     is_online,
-                    get_storage_json,
                     get_filesystem_json,
                     print_json,
-                    reboot
+                    reboot,
+                    get_read_from_opt,
+                    get_blueprint_from_opt,
+                    get_keyboard_json,
+                    read_blueprint
                 ])
                 .run(tauri::generate_context!())
                 .expect("error while running tauri application");
