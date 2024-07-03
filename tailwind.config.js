881bbb9a4f375ca71598930f1533f784f8d4dd3f
naufaladrian <naufal.adrian904@gmail.com>
2024-07-03 12:13:13 +0700

    add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         greenTealinux: "#26A768",
         greyButton: "#929AAB",
         greyBorder: "#E4E7E6",
+        greyVariant: "#D6D6D6",
         userCheckBox: "#757575",
         grayTealinux: "#E7EDED",
         whiteTealinux: "#FFFEFB",
