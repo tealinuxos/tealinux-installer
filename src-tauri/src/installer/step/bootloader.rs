8c2c10b3581b3927ca3357c71bdb2af227a9df82
gagahsyuja <gghssa1@gmail.com>
2024-07-04 15:55:17 +0700

    Adapt to new struct

--- src-tauri/src/installer/step/bootloader.rs
+++ src-tauri/src/installer/step/bootloader.rs
@@ -6,18 +6,42 @@ use super::partitioning::{ get_boot_path, get_boot_mountpoint };
 
 pub fn install_bootloader(blueprint: &BluePrint) -> Result<(), Error>
 {
-    let firmware = bootloader::get_firmware_type();
-
-    match firmware
+    if blueprint.bootloader.is_none()
     {
-        FirmwareKind::UEFI => {
-            let destination = get_boot_mountpoint(blueprint);
-            bootloader::install_grub_bootloader(FirmwareKind::UEFI, None, destination)?;
-        }
+        return Ok(());
+    }
+    else
+    {
+        let firmware = blueprint.bootloader.as_ref().unwrap();
 
-        FirmwareKind::BIOS => {
-            let destination = get_boot_path(blueprint);
-            bootloader::install_grub_bootloader(FirmwareKind::BIOS, destination, None)?;
+        match firmware.firmware_type
+        {
+            FirmwareKind::UEFI => {
+
+                let path = if firmware.path.is_some()
+                {
+                    Some("/boot/efi".to_string())
+                }
+                else
+                {
+                    get_boot_mountpoint(blueprint)
+                };
+
+                bootloader::install_grub_bootloader(FirmwareKind::UEFI, None, path)?;
+            }
+            FirmwareKind::BIOS => {
+
+                let path = if let Some(path) = firmware.path.clone()
+                {
+                    Some(path)
+                }
+                else
+                {
+                    get_boot_path(blueprint)
+                };
+
+                bootloader::install_grub_bootloader(FirmwareKind::BIOS, path, None)?;
+            }
         }
     }
 
