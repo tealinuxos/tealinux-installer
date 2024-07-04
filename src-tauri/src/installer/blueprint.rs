fd011abb0e3d16aac4d6c14f3ce17f080e493c51
gagahsyuja <gghssa1@gmail.com>
2024-07-04 15:52:35 +0700

    Add bootloader struct and rename fields name

--- src-tauri/src/installer/blueprint.rs
+++ src-tauri/src/installer/blueprint.rs
@@ -2,6 +2,7 @@ use serde::{ Serialize, Deserialize };
 use tea_arch_chroot_lib::chroot::Account;
 use tea_arch_chroot_lib::chroot::Timezone;
 use tea_arch_chroot_lib::chroot::Locale;
+use tea_arch_chroot_lib::resource::FirmwareKind;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct BluePrint
@@ -9,7 +10,8 @@ pub struct BluePrint
     pub account: Option<Account>,
     pub locale: Option<Locale>,
     pub timezone: Option<Timezone>,
-    pub disk: Option<Vec<Partition>>
+    pub disk: Option<Vec<Partition>>,
+    pub bootloader: Option<Bootloader>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -17,8 +19,17 @@ pub struct Partition
 {
     pub path: Option<String>,
     pub mountpoint: Option<String>,
-    pub format: Option<String>,
+    pub filesystem: Option<String>,
+    pub format: bool,
     pub start: u64,
     pub end: u64,
     pub size: u64
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct Bootloader
+{
+    pub firmware_type: FirmwareKind,
+    pub path: Option<String>
 }
