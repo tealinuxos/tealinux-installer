ca741ef1726634b3e518c0bbbf5e7a4190f9b5bd
gagahsyuja <gghssa1@gmail.com>
2024-07-05 13:50:11 +0700

    Ignore online status

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,22 +32,15 @@
             <h2 class="font-poppin mb-4 text-4xl tracking-normal">Press start to install</h2>
 
             <div class="p-2">
-                {#await isOnline() then online}
-                    {#if !online}
-                        <h3 class="mb-2 text-red-600 font-bold">Please connect to internet!</h3>
-                        <a href="/installation/locale">Bypass Internet</a>
-                    {/if}
                     <br />
                     <div>
                         <button
                             on:click={() => window.location.href = '/installation'}
                             class=" bg-greenTealinux rounded-3xl hover:bg-green-800 text-white font-poppinsemibold text-xl py-[10px] px-28 disabled:bg-green-400 border-2 border-black/15"
-                            disabled={!online}
                         >
                             START
                         </button>
                     </div>
-                {/await}
             </div>
         {/await}
 	</div>
