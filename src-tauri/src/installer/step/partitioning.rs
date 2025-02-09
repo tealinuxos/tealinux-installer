7470234cd0b703bc993c6683ff3cf3fa81a2cc9c
gagahsyuja <gghssa1@gmail.com>
2025-02-09 09:53:25 +0700

    Unmounting partition before partitioning phase

--- src-tauri/src/installer/step/partitioning.rs
+++ src-tauri/src/installer/step/partitioning.rs
@@ -10,6 +10,8 @@ use std::fs::{create_dir, create_dir_all};
 
 pub fn partitioning(blueprint: &BluePrint) -> Result<(), Error>
 {
+    let _ = cmd!("umount", "--recursive", "/tealinux-mount");
+
     let firmware_type = &blueprint.bootloader.as_ref().unwrap().firmware_type;
     let bootloader_path = &blueprint.bootloader.as_ref().unwrap().path;
 
