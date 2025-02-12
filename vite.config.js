ec7b13f3dfb7f8825977fd0aa99edd12f91afe88
gagahsyuja <gghssa1@gmail.com>
2025-02-12 21:38:15 +0700

    Externalize tauri's plugin-process module explicitly

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { defineConfig } from 'vitest/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            external: [
+                '@tauri-apps/plugin-process'
+            ]
+        }
+    },
 	plugins: [sveltekit(), tailwindcss()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
