eaedde609ec863ebbe358e1b1b212dece7afb78e
naufaladrian <naufal.adrian904@gmail.com>
2025-03-10 09:26:08 +0700

    feat: add glowing text component

---
+++ src/lib/components/ui/GlowingText.svelte
@@ -0,0 +1,16 @@
+<script>
+	let { text } = $props();
+</script>
+
+<div class="relative grid place-items-center">
+	<span
+		class="text-xl font-[400] bg-gradient-to-r from-[#4CDA95] via-[#26A768] to-[#26A768] bg-clip-text text-transparent blur-[1px]"
+	>
+		{text}
+	</span>
+	<span
+		class="absolute text-xl font-[400] bg-gradient-to-r from-[#4CDA95] via-[#26A768] to-[#26A768] bg-clip-text text-transparent"
+	>
+		{text}
+	</span>
+</div>
