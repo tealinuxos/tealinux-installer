887eda30f1ce35c926735f80743ace43c323601c
naufaladrian <naufal.adrian904@gmail.com>
2025-03-10 09:26:31 +0700

    feat: add two side layout

---
+++ src/lib/components/layouts/TwoSide.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let gap = '20px'; // Optional: Add a gap between the sides
+</script>
+
+<div class="flex items-center justify-between h-[648px] p-[15px] text-white">
+	<div class="flex flex-1 items-center justify-center">
+		<slot name="left"></slot>
+	</div>
+	<div class="w-[640px] h-full p-5 font-jakarta">
+		<slot name="right"></slot>
+	</div>
+</div>
