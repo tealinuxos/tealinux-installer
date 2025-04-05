2b951a2ef097402161bd65e005ee491877979a57
naufaladrian <naufal.adrian904@gmail.com>
2025-04-05 21:47:44 +0700

    feat: add Button component with glowing text and customizable size

--- src/lib/components/ui/GlowingText.svelte
+++ src/lib/components/ui/GlowingText.svelte
@@ -1,15 +1,18 @@
 <script>
-	let { text } = $props();
+	let { text, size } = $props();
+
+	// Default values
+	size = size || 'xl'; // Default size is 'xl'
 </script>
 
-<div class="relative grid place-items-center">
+<div class="relative grid place-items-center w-fit">
 	<span
-		class="text-xl font-[400] bg-gradient-to-r from-[#4CDA95] via-[#26A768] to-[#26A768] bg-clip-text text-transparent blur-[1px]"
+		class={`text-${size} font-[400] bg-gradient-to-r from-[#4CDA95] via-[#26A768] to-[#26A768] bg-clip-text text-transparent blur-[1px]`}
 	>
 		{text}
 	</span>
 	<span
-		class="absolute text-xl font-[400] bg-gradient-to-r from-[#4CDA95] via-[#26A768] to-[#26A768] bg-clip-text text-transparent"
+		class={`absolute text-${size} font-[400] bg-gradient-to-r from-[#4CDA95] via-[#26A768] to-[#26A768] bg-clip-text text-transparent`}
 	>
 		{text}
 	</span>
