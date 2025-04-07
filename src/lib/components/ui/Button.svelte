dab8e907b43e270b0e1f52188e3952d5b400308d
naufaladrian <naufal.adrian904@gmail.com>
2025-04-07 19:52:26 +0700

    feat: update Button component styles and GlowingText size for improved UI consistency

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -12,11 +12,11 @@
 
 {#if variant === 'glow'}
 	<button
-		class="cursor-pointer grid place-items-center py-[9px] px-[30px] text-xl bg-[#101010] rounded-sm hover:shadow-[0_0px_5px_1px_#00B85E] transition-all duration-300 border-[0.3px] border-[#3C6350] disabled:cursor-not-allowed"
+		class="cursor-pointer grid place-items-center py-[6px] px-[27px] text-md bg-[#101010] rounded-sm hover:shadow-[0_0px_5px_1px_#00B85E] transition-all duration-300 border-[0.3px] border-[#3C6350] disabled:cursor-not-allowed"
 		disabled={isDisabled}
 		{onclick}
 		aria-label="Go to previous step"
 	>
-		<GlowingText text={btnText} />
+		<GlowingText text={btnText} size={'lg'} />
 	</button>
 {/if}
