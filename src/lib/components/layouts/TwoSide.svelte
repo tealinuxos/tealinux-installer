07f087e39ef08130ff2b6d9b70f2594eb4b06699
naufaladrian <naufal.adrian904@gmail.com>
2025-04-07 17:06:07 +0700

    feat: update TwoSide component to use props for gap and content rendering

--- src/lib/components/layouts/TwoSide.svelte
+++ src/lib/components/layouts/TwoSide.svelte
@@ -1,12 +1,15 @@
 <script>
-	export let gap = '20px'; // Optional: Add a gap between the sides
+	let { gap, left, right } = $props(); // Optional: Add a gap between the sides
+	gap = gap || 'gap-4'; // Default gap if not provided
 </script>
 
 <div class="flex items-center justify-between p-[15px] py-0 text-white">
 	<div class="flex flex-1 items-center justify-center">
-		<slot name="left"></slot>
+		<!-- <slot name="left"/> -->
+		{@render left()}
 	</div>
 	<div class="w-[640px] h-full p-5 font-jakarta">
-		<slot name="right"></slot>
+		<!-- <slot name="right"/> -->
+		{@render right()}
 	</div>
 </div>
