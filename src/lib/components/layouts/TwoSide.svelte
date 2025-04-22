5e4278c410e9a7d1527ed7756b542d332cecf440
naufaladrian <naufal.adrian904@gmail.com>
2025-04-22 08:59:42 +0700

    fix: adjust layout styles in TwoSide component for better alignment and spacing

--- src/lib/components/layouts/TwoSide.svelte
+++ src/lib/components/layouts/TwoSide.svelte
@@ -3,12 +3,12 @@
 	gap = gap || 'gap-4'; // Default gap if not provided
 </script>
 
-<div class="flex items-center justify-between p-[15px] py-0 text-white">
+<div class="flex flex-1 items-center justify-between p-[15px] py-0 text-white">
 	<div class="flex flex-1 items-center justify-center">
 		<!-- <slot name="left"/> -->
 		{@render left()}
 	</div>
-	<div class="w-[640px] h-full p-5 font-jakarta">
+	<div class="w-[640px] h-full py-5 font-jakarta">
 		<!-- <slot name="right"/> -->
 		{@render right()}
 	</div>
