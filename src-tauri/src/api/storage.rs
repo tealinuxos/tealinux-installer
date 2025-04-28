5f1e6a043b52556efa5b66c7051deb2e1233c79b
gagahsyuja <gghssa1@gmail.com>
2025-04-28 22:40:36 +0700

    Set storage instead of partition

--- src-tauri/src/api/storage.rs
+++ src-tauri/src/api/storage.rs
@@ -1,13 +1,16 @@
 use super::partition::blueprint_set_partition;
 use crate::installer::{ Storage, Partition };
+use std::io::Error;
 
 #[tauri::command]
-pub async fn blueprint_set_storage(storage: String, partition: String)
+pub async fn blueprint_set_storage(storage: String)
 {
-    let mut storage: Storage = serde_json::from_str(&storage).unwrap();
+    let storage: Storage = serde_json::from_str(&storage).unwrap();
 
-    let partitions: Vec<Partition> = serde_json::from_str(&partition).unwrap();
-    storage.partitions = Some(partitions);
+    println!("{:#?}", storage);
+
+    // let partitions: Vec<Partition> = serde_json::from_str(&partition).unwrap();
+    // storage.partitions = Some(partitions);
 
     let mut blueprint = super::get_blueprint().unwrap();
 
