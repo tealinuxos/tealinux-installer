ae46f3d27088beeceebed381e642d88a5b04b9a3
naufaladrian <naufal.adrian904@gmail.com>
2025-05-01 17:07:10 +0700

    Update installation layout to remove Navigation component and adjust styling for better responsiveness

--- src/routes/installation/+layout.svelte
+++ src/routes/installation/+layout.svelte
@@ -1,12 +1,7 @@
-<script>
-	import Navigation from '$lib/components/Navigation.svelte';
-</script>
-
 <main
-	class="max-h-[720px] max-w-[1080px] h-[720px] w-[1080px] grid place-items-center bg-tealinux font-jakarta"
+	class="max-h-[720px] max-w-[1080px] min-h-dvh w-[1080px] grid place-items-center bg-tealinux font-jakarta"
 >
-	<div class="flex flex-col justify-between max-h-[720px] min-w-full min-h-full max-w-[1080px]">
+	<div class="flex flex-col justify-between max-h-[720px] min-h-[720px] min-w-full max-w-[1080px]">
 		<slot />
-		<Navigation />
 	</div>
 </main>
