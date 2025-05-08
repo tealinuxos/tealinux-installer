07e82d90c4ee249b2d743fa74d8e6ae086fb48e1
naufaladrian <naufal.adrian904@gmail.com>
2025-05-08 10:22:30 +0700

    Refactor Modal component for improved readability and styling consistency

--- src/routes/installation/localization/Modal.svelte
+++ src/routes/installation/localization/Modal.svelte
@@ -1,42 +1,47 @@
 <script>
-    let {
-        show = $bindable(),
-        keyword = $bindable(),
-        onclick = () => console.log('clicked'),
-        data,
-        title = "This is a title",
-        notFoundMessage = "Message not found",
-        field = null
-    } = $props();
+	let {
+		show = $bindable(),
+		keyword = $bindable(),
+		onclick = () => console.log('clicked'),
+		data,
+		title = 'This is a title',
+		notFoundMessage = 'Message not found',
+		field = null
+	} = $props();
 
-    let filteredData = $state(data);
+	let filteredData = $state(data);
 
 	function filter(term) {
-
 		term = term.toLowerCase();
 
-        if (field) {
-            filteredData = data.filter((e) => e[field].toLowerCase().includes(term));
-        } else {
-            filteredData = data.filter((e) => e.toLowerCase().includes(term));
-        }
+		if (field) {
+			filteredData = data.filter((e) => e[field].toLowerCase().includes(term));
+		} else {
+			filteredData = data.filter((e) => e.toLowerCase().includes(term));
+		}
 	}
 
-    $effect(() => {
-        filter(keyword);
-    })
+	$effect(() => {
+		filter(keyword);
+	});
 </script>
 
 {#if show}
 	<div class="fixed inset-0 flex items-center justify-center backdrop-blur-sm z-80">
-		<div class="absolute inset-0 bg-black/50" onclick={() => (show = false)}></div>
-		<div class="flex flex-col min-w-[434px] max-h-full justify-center items-center p-4 bg-black rounded-lg border border-[#3C6350] shadow-[0_0_10px_rgba(38,167,104,0.25)] overflow-auto z-90">
+		<div
+			style="-webkit-backdrop-filter: blur(10px); backdrop-filter: blur(10px)"
+			class="absolute inset-0 bg-black/50"
+			onclick={() => (show = false)}
+		></div>
+		<div
+			class="flex flex-col min-w-[434px] max-h-full justify-center items-center p-4 bg-black rounded-lg border border-[#3C6350] shadow-[0_0_10px_rgba(38,167,104,0.25)] overflow-auto z-90"
+		>
 			<div class="w-full p-6 z-10">
 				<h2 class="text-xl font-bold mb-4 text-white">{title}</h2>
 				<input
 					type="text"
-                    bind:value={keyword}
-					placeholder="{title}"
+					bind:value={keyword}
+					placeholder={title}
 					class="w-full p-2 border rounded-lg mb-4 bg-[#1c1c1c] text-white"
 				/>
 				<!-- daftar keyboard -->
@@ -48,11 +53,11 @@
 								style="height: 28px; padding: 3px 16px;"
 								onclick={() => onclick(item)}
 							>
-                                {#if field}
-                                    <span>{item[field]}</span>
-                                {:else}
-                                    <span>{item}</span>
-                                {/if}
+								{#if field}
+									<span>{item[field]}</span>
+								{:else}
+									<span>{item}</span>
+								{/if}
 								<span class="text-sm text-gray-400">{item.description || ''}</span>
 							</div>
 						{/each}
