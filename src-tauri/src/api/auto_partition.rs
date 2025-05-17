895db2deac38679b93b6dfe0e73134702e721e5d
Fadhil Riyanto <me@fadev.org>
2025-05-17 18:56:27 +0700

    remove deprecated autogenerated_mode from partitioning & add dualboot backend
    Signed-off-by: Fadhil Riyanto <me@fadev.org>

--- src-tauri/src/api/auto_partition.rs
+++ src-tauri/src/api/auto_partition.rs
@@ -5,6 +5,9 @@ use std::vec::Vec;
 use std::{clone, str::FromStr};
 use tea_partition_generator::single_boot_blockdev::{Blkstuff, SingleBootBlockdevice};
 
+use tea_partition_generator::dual_boot_blockdev::{DualBootBlockdevice, DualbootBlkstuff};
+use tea_partition_generator::core::{PartitionGenerator, TeaPartitionGenerator};
+
 #[tauri::command]
 pub async fn autogen_partition_select_disk(
     blkname: String,
@@ -39,6 +42,42 @@ pub async fn autogen_partition_select_disk(
 
             super::write_blueprint(blueprint).unwrap();
         }
+    } else if mode == "dualboot" {
+        println!(
+            "generating config dualboot {} with fs {} swap: {}",
+            blkname, fs, use_swap
+        );
+
+        let mut ctx: DualbootBlkstuff = DualBootBlockdevice::blockdevice(blkname, fs, use_swap);
+
+        let partition_generator_ctx = TeaPartitionGenerator::new("/dev/sdb".to_string());
+        let (start, end) = partition_generator_ctx.find_empty_space_sector_area();
+
+        if start == 0 && end == 0 {
+            // TODO: Add
+            println!("no empty partition, aborting!");
+            return;
+        }
+
+        let ret = ctx.getresult(start, end);
+
+        if let Ok(ret_val) = ret {
+            // Start werite json
+            let mut blueprint = super::get_blueprint().unwrap();
+
+            blueprint.storage = Some(ret_val.into());
+
+            let bootloader = ctx.gen_current_bootloader();
+            if let Some(bootloader_val) = bootloader {
+                blueprint.bootloader = Some(bootloader_val.into());
+            } else {
+                println!("failed to generate bootloader for autopartitioning.");
+            }
+
+            super::write_blueprint(blueprint).unwrap();
+        }
+    } else {
+        // mode manual, ignored
     }
 
     // You can add more functionality as needed
