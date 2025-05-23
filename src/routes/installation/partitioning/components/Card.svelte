1afc487d1b7ce0778f44c233d5280a3d20acb727
naufaladrian <naufal.adrian904@gmail.com>
2025-05-23 20:00:34 +0700

    Add disable prop to Card component for better user interaction

--- src/routes/installation/partitioning/components/Card.svelte
+++ src/routes/installation/partitioning/components/Card.svelte
@@ -1,34 +1,39 @@
 <script>
-    let {
-        selectedMethod,
-        method,
-        onclick = () => console.log('clicked'),
-        onmouseenter = () => {},
-        onmouseleave = () => {},
-        title = "A title",
-        description = "A description",
-        icon
-    } = $props();
+	let {
+		selectedMethod,
+		method,
+		onclick = () => console.log('clicked'),
+		onmouseenter = () => {},
+		onmouseleave = () => {},
+		title = 'A title',
+		description = 'A description',
+		disable = false,
+		icon
+	} = $props();
 
-    let Method = {
-        SINGLE: "single",
-        DUAL: "dual",
-        MANUAL: "manual"
-    };
+	let Method = {
+		SINGLE: 'single',
+		DUAL: 'dual',
+		MANUAL: 'manual'
+	};
 </script>
 
-<div 
-    class="flex flex-col bg-[#101010] rounded-[14px] p-[10px] w-[190px] h-[167px] cursor-pointer transition-all duration-100
-           {selectedMethod === method ? 
-            'border-[1.3px] border-[#3C6350] shadow-[0_0_9px_#26A768]' : 
-            'border-[1.3px] border-transparent hover:border-[#3C6350] hover:shadow-[0_0_9px_#26A768]'}"
-    onclick={onclick}
-    onmouseenter={onmouseenter}
-    onmouseleave={onmouseleave}
+<div
+	class="flex flex-col bg-[#101010] rounded-[14px] p-[10px] w-[190px] h-[167px] cursor-pointer transition-all duration-100
+           {disable
+		? 'grayscale opacity-70 border-[1.3px] border-transparent cursor-not-allowed!'
+		: selectedMethod === method
+			? 'border-[1.3px] border-[#3C6350] shadow-[0_0_9px_#26A768]'
+			: 'border-[1.3px] border-transparent hover:border-[#3C6350] hover:shadow-[0_0_9px_#26A768]'}"
+	{onclick}
+	{onmouseenter}
+	{onmouseleave}
 >
-    <div class="flex items-center gap-2">
-        {@render icon()}
-        <h3 class="text-white font-bold" style="
+	<div class="flex items-center gap-2">
+		{@render icon()}
+		<h3
+			class="text-white font-bold"
+			style="
             color: #FFF;
             leading-trim: both;
             text-edge: cap;
@@ -38,9 +43,14 @@
             font-weight: 500;
             line-height: normal;
             letter-spacing: -0.6px;
-        ">{title}</h3>
-    </div>
-    <p class="text-white mt-2" style="
+        "
+		>
+			{title}
+		</h3>
+	</div>
+	<p
+		class="text-white mt-2"
+		style="
         color: #FFF;
         leading-trim: both;
         text-edge: cap;
@@ -50,7 +60,8 @@
         font-weight: 200;
         line-height: normal;
         letter-spacing: -0.48px;
-    ">
-        {description}
-    </p>
+    "
+	>
+		{description}
+	</p>
 </div>
