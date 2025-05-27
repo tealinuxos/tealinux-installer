f7613f8f6ca6bd851d2816c074cac5498f994f40
naufaladrian <naufal.adrian904@gmail.com>
2025-05-27 10:08:18 +0700

    feat(Installation): add view transition animations to installation layout

--- src/routes/installation/+layout.svelte
+++ src/routes/installation/+layout.svelte
@@ -22,3 +22,41 @@
 		<Modal />
 	</div>
 </main>
+
+<style>
+	@keyframes fade-in {
+		from {
+			opacity: 0;
+		}
+	}
+
+	@keyframes fade-out {
+		to {
+			opacity: 0;
+		}
+	}
+
+	@keyframes slide-from-right {
+		from {
+			transform: translatex(30px);
+		}
+	}
+
+	@keyframes slide-to-left {
+		to {
+			transform: translateX(-30px);
+		}
+	}
+
+	:root::view-transition-old(root) {
+		animation:
+			90ms linear both fade-out,
+			200ms linear both slide-to-left;
+	}
+
+	:root::view-transition-new(root) {
+		animation:
+			210ms linear both fade-in,
+			200ms linear both slide-from-right;
+	}
+</style>
