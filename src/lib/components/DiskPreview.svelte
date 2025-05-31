e739aa9f73edcc437c2e08c3ff9e81e178e70ad0
gagahsyuja <gghssa1@gmail.com>
2025-06-01 04:00:28 +0700

    Show unallocated partition if no partition table exist

--- src/lib/components/DiskPreview.svelte
+++ src/lib/components/DiskPreview.svelte
@@ -2,9 +2,12 @@
 	import { onMount } from 'svelte';
 	import randomColor from 'randomcolor';
 	import prettyBytes from 'pretty-bytes';
+    import { prettySize } from '../essentials';
 	import { parse } from 'svelte/compiler';
 
 	let { disk, colors, showMountpoint = true, showLabel = false } = $props();
+
+    let noPartitionTable = $state(true);
 
 	const getColors = (disk) => {
 		let generated_colors = [];
@@ -24,7 +27,7 @@
 	};
 
 	onMount(() => {
-		console.log(disk);
+        noPartitionTable = disk?.label === "unknown" ? true : false ?? true;
 		colors = colors ? colors : getColors(disk);
 	});
 </script>
@@ -33,47 +36,74 @@
 	<!-- color bar -->
 	<div class="flex mb-2 h-5 w-full overflow-hidden">
 		<div class="h-full flex overflow-hidden w-full">
-			{#each disk.partitions as partition, i}
-				{@const diskSize = disk.size.slice(0, -1)}
-				{@const partitionSize = partition.size.slice(0, -1)}
-				{@const percentage = (partitionSize / diskSize) * 100}
+            {#key noPartitionTable}
+            {#if noPartitionTable}
+                <div style="width: 100%; background-color: #525252" class="h-full"></div>
+            {:else}
+                {#each disk.partitions as partition, i}
+                    {@const diskSize = disk.size.slice(0, -1)}
+                    {@const partitionSize = partition.size.slice(0, -1)}
+                    {@const percentage = (partitionSize / diskSize) * 100}
+                    {@const path =
+                        partition.partitionPath == null ? 'Unallocated' : partition.partitionPath.slice(5)}
+                    {@const filesystem =
+                        partition.filesystem == null
+                            ? path == 'Unallocated'
+                                ? 'Unallocated'
+                                : 'Unknown'
+                            : partition.filesystem}
+                    {@const color = filesystem ? colors[i] : "#525252"}
 
-				{@const color = colors[i]}
-
-				<div style="width: {percentage}%; background-color: {color}" class="h-full"></div>
-			{/each}
+                    <div style="width: {percentage}%; background-color: {color}" class="h-full"></div>
+                {/each}
+            {/if}
+            {/key}
 		</div>
 	</div>
 
 	<!-- information -->
 	<div class="flex flex-wrap gap-y-2 max-h-[35px] overflow-y-auto mb-4 w-fit">
-		{#each disk.partitions as partition, i}
-			{@const color = colors[i]}
-			{@const prettySize = prettyBytes(parseInt(partition.size) * 512)}
-			{@const path =
-				partition.partitionPath == null ? 'Unallocated' : partition.partitionPath.slice(5)}
-			{@const filesystem =
-				partition.filesystem == null
-					? path == 'Unallocated'
-						? 'Unallocated'
-						: 'Unknown'
-					: partition.filesystem}
-			<div class="flex items-start pr-2 gap-x-[2px]">
-				<div style="background-color: {color}" class="w-2 h-2 rounded-full mt-1"></div>
-				<div class="flex flex-col text-[11px] font-jakarta">
-					<span class="pl-1 font-semibold tracking-wide"
-						>{path}
-						{(partition.mountpoint) && showMountpoint
-							? `- ${partition.mountpoint}`
-							: ''}
-                        {(partition.name) && showLabel
-                            ? `- ${partition.name}`
-                            : ''}
+        {#key noPartitionTable}
+        {#if noPartitionTable}
+            <div class="flex items-start pr-2 gap-x-[2px]">
+                <div style="background-color: #545454" class="w-2 h-2 rounded-full mt-1"></div>
+                <div class="flex flex-col text-[11px] font-jakarta">
+                    <span class="pl-1 font-semibold tracking-wide">
                     </span
-					>
-					<span class="pl-1 uppercase whitespace-nowrap">{prettySize} {filesystem}</span>
-				</div>
-			</div>
-		{/each}
+                    >
+                    <span class="pl-1 uppercase whitespace-nowrap">{prettySize(Number(disk?.size.slice(0, -1)))} Unallocated</span>
+                </div>
+            </div>
+        {:else}
+            {#each disk.partitions as partition, i}
+                {@const color = colors[i]}
+                {@const size = prettySize(partition?.size.slice(0, -1))}
+                {@const path =
+                    partition.partitionPath == null ? 'Unallocated' : partition.partitionPath.slice(5)}
+                {@const filesystem =
+                    partition.filesystem == null
+                        ? path == 'Unallocated'
+                            ? 'Unallocated'
+                            : 'Unknown'
+                        : partition.filesystem}
+                <div class="flex items-start pr-2 gap-x-[2px]">
+                    <div style="background-color: {color}" class="w-2 h-2 rounded-full mt-1"></div>
+                    <div class="flex flex-col text-[11px] font-jakarta">
+                        <span class="pl-1 font-semibold tracking-wide"
+                            >{path}
+                            {(partition.mountpoint) && showMountpoint
+                                ? `- ${partition.mountpoint}`
+                                : ''}
+                            {(partition.name) && showLabel
+                                ? `- ${partition.name}`
+                                : ''}
+                        </span
+                        >
+                        <span class="pl-1 uppercase whitespace-nowrap">{size} {filesystem}</span>
+                    </div>
+                </div>
+            {/each}
+        {/if}
+        {/key}
 	</div>
 {/if}
