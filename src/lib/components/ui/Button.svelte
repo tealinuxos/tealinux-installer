d2490a7bdbf2e7353a3af186fad787eb95519dcc
gagahsyuja <gghssa1@gmail.com>
2025-06-03 10:10:52 +0700

    Lower opacity when button is disabled

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -12,7 +12,7 @@
 
 {#if variant === 'glow'}
 	<button
-		class="cursor-pointer grid place-items-center py-[6px] px-[27px] text-md bg-[#101010] rounded-sm hover:shadow-[0_0px_5px_1px_#00B85E] transition-all duration-300 border-[0.3px] border-[#3C6350] disabled:cursor-not-allowed"
+		class="cursor-pointer grid place-items-center py-[6px] px-[27px] text-md bg-[#101010] disabled:opacity-50 rounded-sm enabled:hover:shadow-[0_0px_5px_1px_#00B85E] transition-all duration-300 border-[0.3px] border-[#3C6350] disabled:cursor-not-allowed"
 		disabled={isDisabled}
 		{onclick}
 		aria-label="Go to previous step"
