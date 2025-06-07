117868662648623d5e6b7626aebcb2b30630c886
gagahsyuja <gghssa1@gmail.com>
2025-06-07 15:43:15 +0700

    Automatically set TEALINUX_BUILD env to dev if not set

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -5,9 +5,22 @@ fn main() {
             tauri_build::build()
         }
         Err(_) => {
-            // Abort build if not set
-            eprintln!("error: Environment variable TEALINUX_BUILD is not set during build. Aborting build. see https://github.com/tealinuxos/tealinux-installer?tab=readme-ov-file#building");
-            std::process::exit(1);
+            // If not set, set to dev
+            println!("cargo:warning=Environment variable TEALINUX_BUILD is not set during build. Automatically set to dev. See https://github.com/tealinuxos/tealinux-installer?tab=readme-ov-file#building");
+            unsafe 
+            {
+                std::env::set_var("TEALINUX_BUILD", "dev");
+            }
+            if let Ok(val) = std::env::var("TEALINUX_BUILD")
+            {
+                println!("cargo:warning=TEALINUX_BUILD is set to: {}", val);
+                tauri_build::build()
+            }
+            else
+            {
+                eprintln!("error: Environment variable TEALINUX_BUILD is not set during build. Aborting build. see https://github.com/tealinuxos/tealinux-installer?tab=readme-ov-file#building");
+                std::process::exit(1);
+            }
         }
     }
 }
