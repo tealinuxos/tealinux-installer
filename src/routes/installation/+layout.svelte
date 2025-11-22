354cda3fc054544a152e5041e940cff66a9780bb
FForFachriza <dimasavrian346@gmail.com>
2025-11-22 17:14:34 +0700

    refactor(fe:"/installation"): use svelte transition api instead viewtransition

--- src/routes/installation/+layout.svelte
+++ src/routes/installation/+layout.svelte
@@ -1,60 +1,27 @@
-<script>
-	import { onNavigate } from '$app/navigation';
-	import Modal from '$lib/components/modals/Modal.svelte';
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing'; 
+	import { page } from '$app/stores';
 
-	onNavigate((navigation) => {
-		if (!document.startViewTransition) return;
+	interface Props {
+		children: Snippet;
+	}
 
-		return new Promise((resolve) => {
-			document.startViewTransition(async () => {
-				resolve();
-				await navigation.complete;
-			});
-		});
-	});
+	let { children }: Props = $props();
 </script>
 
 <main
-	class="max-h-[720px] max-w-[1080px] min-h-dvh w-[1080px] grid place-items-center bg-tealinux font-jakarta"
+	class="max-h-[720px] max-w-[1080px] min-h-dvh w-[1080px] grid place-items-center bg-tealinux font-jakarta overflow-hidden"
 >
-	<div class="flex flex-col justify-between max-h-[720px] min-h-[720px] min-w-full max-w-[1080px]">
-		<slot />
-		<Modal />
+	<div class="grid grid-cols-1 grid-rows-1 max-h-[720px] min-h-[720px] min-w-full max-w-[1080px]">
+		{#key $page.url.pathname}
+			<div
+				in:fade={{ duration: 300, easing: cubicOut }}
+				class="col-start-1 row-start-1 flex flex-col justify-between w-full h-full bg-tealinux"
+			>
+				{@render children?.()}
+			</div>
+		{/key}
 	</div>
 </main>
-
-<style>
-	@keyframes fade-in {
-		from {
-			opacity: 0;
-		}
-	}
-
-	@keyframes fade-out {
-		to {
-			opacity: 0;
-		}
-	}
-
-	/* @keyframes slide-from-right {
-		from {
-			transform: translatex(30px);
-		}
-	}
-
-	@keyframes slide-to-left {
-		to {
-			transform: translateX(-30px);
-		}
-	} */
-
-	:root::view-transition-old(root) {
-		animation: 300ms linear both fade-out;
-		/* 200ms linear both slide-to-left; */
-	}
-
-	:root::view-transition-new(root) {
-		animation: 300ms linear both fade-in;
-		/* 200ms linear both slide-from-right; */
-	}
-</style>
