28802293ba229e54cdf1ba7acea1d6398b069610
FForFachriza <dimasavrian346@gmail.com>
2025-11-26 19:06:27 +0700

    fix(fe "/installation"): use page from app state instead from app stores due to deprecated module

--- src/routes/installation/+layout.svelte
+++ src/routes/installation/+layout.svelte
@@ -2,7 +2,7 @@
 	import type { Snippet } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import { cubicOut } from 'svelte/easing';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import Modal from '$lib/components/modals/Modal.svelte';
 
 	interface Props {
@@ -16,7 +16,7 @@
 	class="max-h-[720px] max-w-[1080px] min-h-dvh w-[1080px] grid place-items-center bg-tealinux font-jakarta overflow-hidden"
 >
 	<div class="grid grid-cols-1 grid-rows-1 max-h-[720px] min-h-[720px] min-w-full max-w-[1080px]">
-		{#key $page.url.pathname}
+		{#key page.url.pathname}
 			<div
 				in:fade={{ duration: 300, easing: cubicOut }}
 				class="col-start-1 row-start-1 flex flex-col justify-between w-full h-full bg-tealinux"
