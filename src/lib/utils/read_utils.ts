509aeb27555153987c80ae4945a9e71b72e9e22e
FForFachriza <dimasavrian346@gmail.com>
2025-11-27 01:41:11 +0700

    feat (fe/utils): add getKeyboardJson, getLocaleJson, getTimezoneJSON to implement localization type

--- src/lib/utils/read_utils.ts
+++ src/lib/utils/read_utils.ts
@@ -1,5 +1,8 @@
-import { commands, type BluePrint } from '$types/commands';
+import { commands, type BluePrint} from '$types/commands';
+import type { Keyboard } from '$types/keyboard-variant';
+import type { Locale } from '$types/locale-variant';
 import type { ReadFromOpt } from '$types/read-from-opt';
+import type { Timezone } from '$types/timezone-variant';
 
 export const getSystemInfo = async (): Promise<ReadFromOpt | null> => {
 	try {
@@ -19,6 +22,39 @@ export const getBlueprintInfo = async (): Promise<BluePrint | null> => {
 		return json;
 	} catch (error) {
 		console.error('Error fetching blueprint info:', error);
+		return null;
+	}
+};
+
+export const getKeyboardJSON = async (): Promise<Keyboard[] | null> => {
+	try {
+		const data = await commands.getKeyboardJson();
+		const json: Keyboard[] = typeof data === 'string' ? JSON.parse(data) : data;
+		return json;
+	} catch (error) {
+		console.error('Error fetching keyboard info:', error);
+		return null;
+	}
+};
+
+export const getLocaleJSON = async (): Promise<Locale[] | null> => {
+	try {
+		const data = await commands.getLocaleJson();
+		const json: Locale[] = typeof data === 'string' ? JSON.parse(data) : data;
+		return json;
+	} catch (error) {
+		console.error('Error fetching locale info:', error);
+		return null;
+	}
+};
+
+export const getTimezoneJSON = async (): Promise<Timezone[] | null> => {
+	try {
+		const data = await commands.getTimezoneJson();
+		const json: Timezone[] = typeof data === 'string' ? JSON.parse(data) : data;
+		return json;
+	} catch (error) {
+		console.error('Error fetching timezone info:', error);
 		return null;
 	}
 };
