9013f1422e24cda2e68aa7371e7635a1e66a7e50
Fadhil Riyanto <me@fadev.org>
2025-11-27 11:15:50 +0700

    refactor get_read_json, get_blueprint_from_opt, set_read_json, set_empty_blueprint
    Signed-off-by: Fadhil Riyanto <me@fadev.org>

--- src-tauri/src/api/mod.rs
+++ src-tauri/src/api/mod.rs
@@ -1,42 +1,55 @@
-use crate::read::{get_read, Read};
-use crate::installer::BluePrint;
-use std::fs::{ File, create_dir_all, read_to_string };
-use std::io::{ Write, BufReader, BufWriter, Error };
 use super::storage::filesystem::filesystem_list;
-use std::path::Path;
+use crate::installer::BluePrint;
+use crate::read::{get_read, Read};
+use crate::utils::error_handler;
+use crate::utils::error_handler::ErrorHandler;
 use os::get_other_os;
+use std::fs::{create_dir_all, read_to_string, File};
+use std::io::{BufReader, BufWriter, Error, Write};
+use std::path::Path;
 use tea_arch_chroot_lib::chroot::bootloader::get_firmware_type;
 use tea_arch_chroot_lib::resource::Keyboard;
 
-pub mod locale;
-pub mod timezone;
-pub mod keyboard;
 pub mod account;
-pub mod firmware;
-pub mod storage;
-pub mod partition;
 pub mod auto_partition;
+pub mod firmware;
+pub mod keyboard;
+pub mod locale;
 pub mod os;
+pub mod partition;
+pub mod storage;
+pub mod timezone;
 
 #[tauri::command]
 #[specta::specta]
-pub async fn get_read_json() -> String
-{
+pub async fn get_read_json() -> Result<String, error_handler::ErrorHandlerEmitter> {
     let read = get_read();
 
-    let json = serde_json::to_string_pretty(&read);
+    let json: Result<String, serde_json::Error> = serde_json::to_string_pretty(&read);
+    match json {
+        Ok(json_ok) => return Ok(json_ok),
+        Err(json_err) => {
+            let ret = error_handler::InternalErrorHandlerEmitter::new(
+                "Error",
+                "Failed to convert system info into json",
+                error_handler::ErrorHandlerServerity::LOW,
+                Box::new(json_err),
+            );
 
-    json.unwrap()
+            // ret.record_log();
+            return Err(ret.get_return());
+        }
+    }
+
+    // json.unwrap()
 }
 
 #[tauri::command]
 #[specta::specta]
-pub async fn get_read_from_opt() -> String
-{
+pub async fn get_read_from_opt() -> String {
     let path = "/opt/tea-installer/read.json";
 
-    match read_to_string(path)
-    {
+    match read_to_string(path) {
         Ok(json) => json,
         Err(_) => {
             self::set_read_json().await;
@@ -47,16 +60,20 @@ pub async fn get_read_from_opt() -> String
 
 #[tauri::command]
 #[specta::specta]
-pub async fn get_blueprint_from_opt() -> String
-{
+pub async fn get_blueprint_from_opt() -> String {
     let path = "/opt/tea-installer/installer.json";
 
-    match read_to_string(path)
-    {
+    match read_to_string(path) {
         Ok(json) => json,
         Err(_) => {
-
-            let blueprint = BluePrint { account: None, locale: None, timezone: None, storage: None, bootloader: None, keyboard: None };
+            let blueprint = BluePrint {
+                account: None,
+                locale: None,
+                timezone: None,
+                storage: None,
+                bootloader: None,
+                keyboard: None,
+            };
             let mut file = File::create("/opt/tea-installer/installer.json").unwrap();
 
             let json = serde_json::to_string_pretty(&blueprint).unwrap();
@@ -72,44 +89,106 @@ pub async fn get_blueprint_from_opt() -> String
 
 #[tauri::command]
 #[specta::specta]
-pub async fn set_read_json()
-{
+pub async fn set_read_json() -> Result<(), error_handler::ErrorHandlerEmitter> {
     let json = self::get_read_json().await;
 
-    let path = Path::new("/opt/tea-installer/");
+    match json {
+        Ok(json_ok) => {
+            let path = Path::new("/opt/tea-installer/");
 
-    if !path.exists()
-    {
-        create_dir_all("/opt/tea-installer/").unwrap();
+            if !path.exists() {
+                let ret = create_dir_all("/opt/tea-installer/");
+                if let Err(ret_err) = ret {
+                    let err_ret = error_handler::InternalErrorHandlerEmitter::new(
+                        "Error",
+                        "Failed to create a directory at /opt/tea-installer/",
+                        error_handler::ErrorHandlerServerity::HIGH,
+                        Box::new(ret_err),
+                    );
+
+                    return Err(err_ret.get_return());
+                }
+            }
+
+            let file = File::create("/opt/tea-installer/read.json");
+
+            match file {
+                Ok(mut file_ok) => {
+                    file_ok.write_fmt(format_args!("{}", json_ok)).unwrap();
+                    return Ok(());
+                }
+                Err(file_err) => {
+                    let err_ret = error_handler::InternalErrorHandlerEmitter::new(
+                        "Error",
+                        "Failed to create a file /opt/tea-installer/read.json",
+                        error_handler::ErrorHandlerServerity::HIGH,
+                        Box::new(file_err),
+                    );
+
+                    return Err(err_ret.get_return());
+                }
+            }
+        }
+        Err(json_err) => {
+            return Err(json_err);
+        }
     }
-
-    let mut file = File::create("/opt/tea-installer/read.json").unwrap();
-
-    file.write_fmt(format_args!("{}", json)).unwrap();
 }
 
 #[tauri::command]
 #[specta::specta]
-pub async fn set_empty_blueprint()
-{
+pub async fn set_empty_blueprint() -> Result<(), error_handler::ErrorHandlerEmitter> {
     let path = Path::new("/opt/tea-installer/");
 
-    if !path.exists()
-    {
-        create_dir_all("/opt/tea-installer/").unwrap();
+    if !path.exists() {
+        let ret = create_dir_all("/opt/tea-installer/");
+        if let Err(ret_err) = ret {
+            let err_ret = error_handler::InternalErrorHandlerEmitter::new(
+                "Error",
+                "Failed to create a directory at /opt/tea-installer/",
+                error_handler::ErrorHandlerServerity::HIGH,
+                Box::new(ret_err),
+            );
+
+            return Err(err_ret.get_return());
+        }
+        // create_dir_all("/opt/tea-installer/").unwrap();
     }
 
-    let mut file = File::create("/opt/tea-installer/installer.json").unwrap();
+    let file = File::create("/opt/tea-installer/installer.json");
 
-    let blueprint = BluePrint { account: None, locale: None, timezone: None, storage: None, bootloader: None, keyboard: None };
+    match file {
+        Ok(mut file_ok) => {
+            let blueprint = BluePrint {
+                account: None,
+                locale: None,
+                timezone: None,
+                storage: None,
+                bootloader: None,
+                keyboard: None,
+            };
 
-    let blueprint_json = serde_json::to_string_pretty(&blueprint).unwrap();
+            let blueprint_json = serde_json::to_string_pretty(&blueprint).unwrap();
 
-    file.write_fmt(format_args!("{}", blueprint_json)).unwrap();
+            file_ok
+                .write_fmt(format_args!("{}", blueprint_json))
+                .unwrap();
+        }
+        Err(file_err) => {
+            let err_ret = error_handler::InternalErrorHandlerEmitter::new(
+                "Error",
+                "Failed to create a file /opt/tea-installer/installer.json",
+                error_handler::ErrorHandlerServerity::HIGH,
+                Box::new(file_err),
+            );
+
+            return Err(err_ret.get_return());
+        }
+    }
+    return Ok(());
 }
 
-pub fn get_blueprint() -> Result<BluePrint, Error>
-{
+pub fn get_blueprint() -> Result<BluePrint, Error> {
     let file = File::open("/opt/tea-installer/installer.json")?;
     let reader = BufReader::new(file);
 
@@ -118,10 +197,9 @@ pub fn get_blueprint() -> Result<BluePrint, Error>
     Ok(blueprint)
 }
 
-pub fn write_blueprint(blueprint: BluePrint) -> Result<(), Error>
-{
+pub fn write_blueprint(blueprint: BluePrint) -> Result<(), Error> {
     let blueprint = serde_json::to_string_pretty(&blueprint)?;
-    
+
     let file = File::create("/opt/tea-installer/installer.json")?;
     let mut writer = BufWriter::new(file);
 
@@ -130,10 +208,9 @@ pub fn write_blueprint(blueprint: BluePrint) -> Result<(), Error>
     Ok(())
 }
 
-pub fn write_read(read: Read) -> Result<(), Error>
-{
+pub fn write_read(read: Read) -> Result<(), Error> {
     let read = serde_json::to_string_pretty(&read)?;
-    
+
     let file = File::create("/opt/tea-installer/read.json")?;
     let mut writer = BufWriter::new(file);
 
@@ -144,8 +221,7 @@ pub fn write_read(read: Read) -> Result<(), Error>
 
 #[tauri::command]
 #[specta::specta]
-pub async fn get_filesystem_json() -> String
-{
+pub async fn get_filesystem_json() -> String {
     let filesystem = filesystem_list();
 
     serde_json::to_string_pretty(&filesystem).unwrap()
@@ -153,8 +229,7 @@ pub async fn get_filesystem_json() -> String
 
 #[tauri::command]
 #[specta::specta]
-pub async fn read_blueprint()
-{
+pub async fn read_blueprint() {
     let blueprint = self::get_blueprint().unwrap();
 
     println!("{:#?}", blueprint);
@@ -162,15 +237,12 @@ pub async fn read_blueprint()
 
 #[tauri::command]
 #[specta::specta]
-pub async fn get_other_os_json() -> String
-{
+pub async fn get_other_os_json() -> String {
     let other_os = get_other_os();
 
-    if let Some(o) = other_os
-    {
+    if let Some(o) = other_os {
         serde_json::to_string_pretty(&o).unwrap()
-    }
-    else {
+    } else {
         String::from("{}")
     }
 }
